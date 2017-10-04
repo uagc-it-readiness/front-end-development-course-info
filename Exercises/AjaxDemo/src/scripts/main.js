7d335f025f5b2f4c412088ee9645c8280375c0a6
Daniel Worthy <daniel.worthy@rockfishinteractive.com>
2017-10-03 19:19:32 -0500

    Adding

--- Exercises/AjaxDemo/src/scripts/main.js
+++ Exercises/AjaxDemo/src/scripts/main.js
@@ -1,20 +1,77 @@
 $(document).ready(function () {
-    var url = 'http://localhost:3004/cart'
+    var url = 'http://localhost:3004/cart';
 
-    $(document).ready(function () {
-      
-    });
+    $('#btnAddToCart').on('click', addToCart);
+    // $('.remove').on('click', remove);
+    $('#main').on('click', '.remove', remove);
+    $('#main').on('click', '.addMore', addMore);
+
+
+    // console.log($('.me').parents('.parent'));
+    // console.log($('.me').closest('.main'));
+
+    loadCart();
 
     function addToCart() {
-       
+        var product = $('#product');
+        var item = {
+            name: product.find('.name').val(),
+            qty: parseInt(product.find('.qty').val()),
+            img: product.find('.img').val()
+        };
+        console.log(item);
+
+        $.post(url, item, function () {
+            loadCart();
+        });
+
     }
 
     function loadCart() {
-
+        $.get(url, function (response) {
+            var html = convertResponseToHTML(response);
+            $('#main').html(html);
+        });
     }
 
     function addMore() {
-       
+        var btn = $(this);
+        var container = btn.closest('.media');
+        var id = container.find('.id').val();
+        var newQty = parseInt(container.find('.qty').val()) + 1;
+
+        $.ajax({
+            url: url + '/' + id,
+            method: 'PATCH',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                qty: newQty
+            }),
+            success: function () {
+                loadCart();
+            }
+        });
+
+
+
+    }
+
+    function remove() {
+        var btn = $(this);
+        var container = btn.closest('.media');
+        var id = container.find('.id').val();
+
+        $.ajax({
+            url: url + '/' + id,
+            method: 'delete',
+            success: function () {
+                loadCart();
+            }
+
+        });
+
+
+
     }
 
     function convertResponseToHTML(resp) {
@@ -37,7 +94,7 @@ $(document).ready(function () {
 
             </div>
           </div>
-            `
+            `;
 
             html += cartHtml;
         }
