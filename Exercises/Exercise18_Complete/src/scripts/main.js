7c8e38707a856d5e5438eaee08a0c686a86da755
Middaugh <Jonathan.Middaugh@gmail.com>
2018-04-19 14:14:10 -0500

    updating session 26 and adding E18 complete

---
+++ Exercises/Exercise18_Complete/src/scripts/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+    //List Titles
+    var appendUrl = 'http://jsonplaceholder.typicode.com/posts?userId=1';
+	
+	$.ajax({
+            url:appendUrl,
+            method:'GET',
+            success: function(response){
+				let appendEl = $('#append');
+				for(var i = 0; i < response.length; i++){
+					appendEl.append('<li>Index: '+i+' Title: '+response[i].title+'</li>');
+				}
+			},
+            failure:function(response){}
+        });
+
+
+    //
+    var prependUrl = 'http://jsonplaceholder.typicode.com/photos?albumId=1';
+	$.ajax({
+            url:prependUrl,
+            method:'GET',
+            success: function(response){
+				let prependEl = $('#prepend');
+				for(var i = 0; i < response.length; i++){
+					prependEl.append('<li>Index: '+i+' Title: '+response[i].title+'</li>');
+				}
+			},
+            failure:function(response){}
+        });
+
+});
